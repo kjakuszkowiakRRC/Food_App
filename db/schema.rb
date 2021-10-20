8039a688c2d92bb0801bd28df5412e84e28b651f
Kevin <kevin.jakuszkowiak@gmail.com>
2021-10-20 17:09:56 -0500

    added locations model to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_203408) do
+ActiveRecord::Schema.define(version: 2021_10_20_220552) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "province"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
